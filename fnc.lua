9fa7f516498ba6dcec7e656bf3322bc03eaacf45
xxx14445551x <dimastas811@gmail.com>
2026-04-18 17:55:31 +0800

    Update fnc.lua

--- fnc.lua
+++ fnc.lua
@@ -5,10 +5,38 @@ if not req then
     warn("no function http")
 end 
 
-_G.webhook = function(hook, mes)
+_G.webhook = function(hook, color, name , mes)
+    local colors = {
+        black = 0x000000,
+        white = 0xFFFFFF,
+        yellow = 0xFFFF00,
+        cyan = 0x00FFFF,
+        green = 0x00FF00,
+        red = 0xFF0000,
+        blue = 0x0000FF,
+        orange = 0xFFA500,
+        purple = 0x800080,
+        pink = 0xFFC0CB,
+        brown = 0x8B4513,
+        lime = 0x32CD32,
+        navy = 0x000080,
+        gold = 0xFFD700,
+        silver = 0xC0C0C0,
+        gray = 0x808080
+    }
+
+    local emcolor = colors.dark
+
+    if color and type(color) == "string" then
+        local colorkey = string.lower(color) 
+        emcolor = colors[colorkey] or colors.dark
+    end
+
     local data = {
+        username = name .. " ✦",
         embeds = {{
-            description = mes
+            description = mes,
+            color = emcolor
         }}
     }
     local json = http:JSONEncode(data)
@@ -19,4 +47,3 @@ _G.webhook = function(hook, mes)
         Body = json
     })
 end
-
